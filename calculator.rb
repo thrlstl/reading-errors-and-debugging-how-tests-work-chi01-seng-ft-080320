86e2f973025090c1e2c11e75f5cd87c7a7761e23
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-23 03:36:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,5 @@ first_number = 5
 second_number = 10
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
